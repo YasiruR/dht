e6ebceb2aff596d059bec3e4114bbd35fa1c5e48
Yasi <wyasirurathsara@gmail.com>
2021-10-23 21:44:05 +0200

    added join to initiator

--- scripts/initiator.sh
+++ scripts/initiator.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 num_nodes=$1
+host=$2
 for node in $(cat node_list.txt | head -n $num_nodes); do
     echo "$node"
     ssh -f "$node" "cd dht/ && ./dht"
+done
+
+for node in $(cat node_list.txt | head -n $num_nodes); do
+    # shellcheck disable=SC1073
+    if [ "$node" == "$host" ]
+    then
+      continue
+    fi
+    curl -X POST "${node}:52520/join?nprime=${host}:52520"
 done
